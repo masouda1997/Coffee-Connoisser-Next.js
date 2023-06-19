0303ee65fb33112355ffe6828ce95024c9d8869e
masoud <masoud30376@gmail.com>
2023-06-19 14:02:47 +0330

    add font but not the way i wanted

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
 		console.log("banner button click");
 	};
 	return (
-		<main className={st.main}>
+		<main className={`${st.main}`}>
 			<div className={st2.heroSec}>
 				<Banner
 					onClick={handleOnBannerButtonClick}
